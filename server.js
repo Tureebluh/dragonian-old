46d86861f11938283a64031923f873d7ce45c4dd
Tureebluh <jarek.thomas@outlook.com>
2018-07-14 11:15:06 -0700

    working on adding contest creation and role authorization going along with that

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import express from 'express';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
 import passport from './steampassport';
+import dbpool from './dbpool';
 
 const server = express();
 
@@ -40,6 +41,7 @@ server.use(passport.session());
 
 server.use((req,res,next) => {
     res.locals.user = req.user;
+    res.locals.roles = req.session.roles;
     next();
 });
 
@@ -57,6 +59,22 @@ server.get('/collabs', (req, res) => {
 });
 server.get('/contest', (req, res) => {
     if(req.isAuthenticated()){
+        if(!req.session.roles){
+            dbpool.getConnection( (err, connection) => {
+                // Use the connection
+                connection.query('CALL Get_UserRoles(' + req.user + ');', (error, results, fields) => {
+                    let tempArray = results[0];
+                    let rolesArray = [];
+                    tempArray.forEach((obj)=>{
+                        rolesArray.push(obj.role);
+                    });
+                    req.session.roles = rolesArray;
+                    connection.release();
+                    if (error) throw error;
+                    // Don't use the connection here, it has been returned to the pool.
+                });
+            });
+        }   
         res.render('contest');
     } else {
         res.redirect('/auth/login');
